dbd137617885706f25c8a23728f8fe8bc01e639a
Eirik.Skjerve <eirik.skjerve@student.uib.no>
2024-03-25 14:10:51 +0100

    Written basic skeleton for piece class

---
+++ Game/piece.py
@@ -0,0 +1,37 @@
+class Piece:
+
+    def __init__(self, name, value, position, color):
+        
+        '''
+        Initializes a piece on the board
+
+        Inputs: 
+        name of piece (string)
+        value of piece (int)
+        position of piece (tuple)
+        
+        '''
+        self.name = name
+        self.value = value
+        self.position = position
+        self.color = color
+        self.set_image()
+
+    def set_image(self):
+        path = "./Game/Images/"
+        match self.name:
+            case "Pawn":
+                pass
+            case "Knight":
+                pass
+            case "Bishop":
+                pass
+            case "Rook":
+                pass
+            case "Queen":
+                pass
+            case "King":
+                pass
+
+        path += "Untitled.png"
+        self.image = path
