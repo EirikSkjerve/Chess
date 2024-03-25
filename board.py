3b316f34b79b23696b6c41623d060e7c11fbdb49
Eirik.Skjerve <eirik.skjerve@student.uib.no>
2024-03-25 14:10:26 +0100

    Written basic board structure with setter and getter

---
+++ board.py
@@ -0,0 +1,40 @@
+class Board:
+
+    def __init__(self):
+        self.board = [[None]*8,
+                      [None]*8,
+                      [None]*8,
+                      [None]*8,
+                      [None]*8,
+                      [None]*8,
+                      [None]*8,
+                      [None]*8]
+
+        self.let_to_num = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
+
+    def get(self,coors):
+        x, y = self.strCoor_to_numCoor(coors)
+        if not(0 < x < 7) or not(0 < y < 7):
+            raise ValueError
+        return self.board[x][y]
+
+    def set(self, coors, piece):
+        x, y = self.strCoor_to_numCoor(coors)
+        if not(0 < x < 7) or not(0 < y < 7):
+            raise ValueError
+        self.board[x][y] = piece
+
+    def strCoor_to_numCoor(self, coors):
+        x, y = 7-(int(coors[1])-1) ,self.let_to_num.get(coors[0])
+        return (x, y)
+
+    def print_board(self):
+        for row in self.board:
+            print(row)
+
+
+
+if __name__ == "__main__":
+    testBoard = Board()
+    testBoard.set('b3', 'pawn')
+    testBoard.print_board()
