4bb4fc6dbc6d32cf384f2254e04bccfd7c461e62
Eirik.Skjerve <eirik.skjerve@student.uib.no>
2024-03-25 12:37:26 +0100

    Grid looks fine

--- main.py
+++ main.py
@@ -7,24 +7,32 @@ def menu():
 
 # if board is none, draw empty grid
 def draw_board(board=None):
-    square_size = max(SCREEN_WIDTH-100, SCREEN_HEIGHT-100)//8
+
+    # depending on what color the player is, this should be false/true for white/black
     draw_dark = True
-    for x in range(0, max(SCREEN_WIDTH-100, SCREEN_HEIGHT-100), square_size):
-        for y in range(0, max(SCREEN_HEIGHT-100, SCREEN_WIDTH -100), square_size):
-            rect = pygame.Rect(x,y,square_size,square_size)
+    for x in range(OFFSET_X, BOARD_DIM + OFFSET_X, SQUARE_SIZE):
+        
+        draw_dark = not draw_dark
+        for y in range(OFFSET_Y, BOARD_DIM + OFFSET_Y, SQUARE_SIZE):
+            rect = pygame.Rect(x,y,SQUARE_SIZE, SQUARE_SIZE)
             if draw_dark:
-                pygame.draw.rect(SCREEN, dark_square_color,rect,square_size)
-                draw_dark = False
+                pygame.draw.rect(SCREEN, dark_square_color,rect,SQUARE_SIZE)
+                draw_dark = not draw_dark
             else:
-                pygame.draw.rect(SCREEN, light_square_color,rect,square_size)
-                draw_dark = True
+                pygame.draw.rect(SCREEN, light_square_color,rect,SQUARE_SIZE)
+                draw_dark = not draw_dark
+
 def main():
-    global SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN, dark_square_color, light_square_color
+    global SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN, dark_square_color, light_square_color, SQUARE_SIZE, OFFSET_X, OFFSET_Y, BOARD_DIM
     SCREEN_WIDTH = 1500
     SCREEN_HEIGHT = 1000
     dark_square_color = (64, 64, 64)
     light_square_color = (200, 200, 200)
 
+    SQUARE_SIZE = min(SCREEN_WIDTH-150, SCREEN_HEIGHT-150)//8
+    OFFSET_X = 75
+    OFFSET_Y = (SCREEN_HEIGHT-(8*SQUARE_SIZE))//2
+    BOARD_DIM = SQUARE_SIZE*8
     pygame.init()
 
     # SCREEN is now the "surface" object on which to draw on
