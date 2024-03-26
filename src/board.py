ef220dbbeff74adb2cbdb450234e71295fe9db92
Eirik.Skjerve <eirik.skjerve@student.uib.no>
2024-03-26 15:06:04 +0100

    Implemented much better coordinate system that should scale better

--- src/board.py
+++ src/board.py
@@ -1,6 +1,6 @@
 class Board:
 
-    def __init__(self):
+    def __init__(self, SQUARE_SIZE, OFFSET_X, OFFSET_Y):
         self.board = [[None]*8,
                       [None]*8,
                       [None]*8,
@@ -20,11 +20,18 @@ class Board:
             black_tile = not black_tile
 
             for num in range(8):
-                tile = Tile("b" if black_tile else "w", let+str(num+1))
+                
+                ul = (SQUARE_SIZE*num+OFFSET_X, SQUARE_SIZE*i+OFFSET_Y)
+                ur = (SQUARE_SIZE*(num+1)+OFFSET_X, SQUARE_SIZE*i+OFFSET_Y)
+                dl = (SQUARE_SIZE*num+OFFSET_X, SQUARE_SIZE*(i+1)+OFFSET_Y)
+                dr = (SQUARE_SIZE*(num+1)+OFFSET_X, SQUARE_SIZE*(i+1)+OFFSET_Y)
+
+                tile = Tile("b" if black_tile else "w", let+str(num+1), (ul, ur, dl, dr))
                 self.board[i][num] = tile
                 black_tile = not black_tile
                 if i == 0 or num == 3:
                     tile.piece = "Pawn"
+
     def get(self,coors):
         x, y = self.strCoor_to_numCoor(coors)
         if not(0 < x < 7) or not(0 < y < 7):
@@ -47,10 +54,18 @@ class Board:
 
 class Tile():
 
-    def __init__(self, color, coordinate, piece=None):
+    def __init__(self, color, coordinate, screen_coordinates, piece=None):
         self.color = color
         self.coordinate = coordinate
         self.piece = piece
+        # this needs to be well defined. screen_coordinates = (a, b, c, d)
+        ul,ur,dl,dr = screen_coordinates  # as (x, y) tuple
+        self.ul = ul  # up left
+        self.ur = ur  # up right
+        self.dl = dl  # down left
+        self.dr = dr  # down right
+
+
 
 if __name__ == "__main__":
     testBoard = Board()
